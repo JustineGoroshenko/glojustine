24e43ebac3cded1b62b05144bc6e537b1250a893
JustineGoroshenko <justinegoroshenko@yahoo.com>
2020-04-02 16:30:15 +0100

    Added advancedd04

--- Advanced Glo/advancedscript.js
+++ Advanced Glo/advancedscript.js
@@ -1,22 +1,19 @@
 'use strict';
 
 
-//1
-let lang = prompt("Please choose your language", 'Eng/Es');
- console.log('Your chosen language: ' + lang);
+//4.1
 
-if(lang == 'eng'){
-   console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
-} else if (lang == 'es'){
-   console.log('lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo');
-} else { console.log('please choose your language');}
 
-//2
-let namePerson = prompt("Authorization", 'your name');
- console.log('Your chosen name: ' + namePerson);
+   let str ='';
+function bonbon(object){
+   if(typeof object !== 'string'){
+      alert('Not a string!');
+   } else {
+        str = object.trim().slice(0, 30) + "...";
+       return str;
+   }
+}
 
-if(namePerson == 'Max'){
-   console.log('Teacher');
-} else if ( namePerson == 'Artem'){
-   console.log('CEO');
-} else { console.log('Student');}
+bonbon('   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem quod unde enim dolorem iusto modi voluptate!            ');
+bonbon(5);
+console.log(str);
