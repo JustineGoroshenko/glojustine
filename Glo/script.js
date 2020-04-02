a97ba8a42e1dbd05a4fcb7c947f501e3ba94bd41
JustineGoroshenko <justinegoroshenko@yahoo.com>
2020-04-02 15:02:45 +0100

    Lesson04 added

--- Glo/script.js
+++ Glo/script.js
@@ -1,60 +1,59 @@
 'use strict';
 
 let money = '2000',
-income = "job",
-deposit = true,
+income = "",
 mission = '5000',
-period = 12,
-budgetPerDay = (money/30),
-addExpenses = 'Taxes, Scholarship, Food';
-//1
+period = '',
+budgetPerDay = '',
+addExpenses = '';
+//1 
+
 console.log(typeof deposit);
 console.log(typeof income);
 console.log(typeof money);
-//2
-console.log(addExpenses.length);
-//3
-console.log('Период равен ' + period + ' месяцев и Цель заработать ' + mission + ' Euro');
-//4
-console.log(addExpenses.toLowerCase().split(', '));
-//5
-console.log(budgetPerDay + ' euro') ;
-console.log('Thank you for your time')// :)
-
-
-
-
 //3.2
-money = +prompt('Your monthly earnings?');
-console.log(money);
+money = +prompt('Your monthly earnings?', '1000');
+console.log('Your monthly earnings are: ' + money + '£');
 //3.3
-addExpenses = prompt('Please, list your potential expenditure');
+addExpenses = prompt('Please, list your potential expenditure', "coca, para, chico, loco");
 console.log(addExpenses.toLowerCase().split(', '));
-//3.4
-deposit = confirm('Press ok if you have a saving account');
-console.log(deposit);
 //3.5
-let mainExpenditures = prompt('Please list your main expenditures'),
-    secExpenditures = prompt('Please list your secondary expenditures'),
-    mainExpenses = +prompt('Amount of the main expenses'),
-    secExpenses = +prompt('Amount of secondary expenses'),
-//3.6 
-    budgetMonth = money - (mainExpenses + secExpenses);
-    console.log(budgetMonth);
-//3.7
-   period =  Math.ceil((mission/budgetMonth));
-   console.log('Goal will be achieved in ' + period + ' months');
-//3.8
-   budgetPerDay = Math.floor(budgetMonth/30);
-   console.log('Daily budget is: ' + budgetPerDay + '£ per day');
-//3.9
-if( budgetPerDay > 120 ){
-   console.log("you have a high amount of income");
+let mainExpenses = +prompt('Amount of the main expenses', '200'),
+    secExpenses = +prompt('Amount of secondary expenses', '100');
+//4.1
+
+let expenses = ''; 
+function getExpenseMonth(a, b) {
+    expenses = a + b;
+   return expenses;
 }
-else if(  budgetPerDay >= 60 && budgetPerDay <= 120){
-   console.log("you have a moderate amount of income");
-} else if( budgetPerDay >= 0 && budgetPerDay <= 60){
-   console.log("you have a low amount of income");
-} else if(budgetPerDay <= 0) {
-   console.log("ups");
+getExpenseMonth(mainExpenses, secExpenses);
+
+/*  Not working, WHY???
+function getExpenseMonth(){
+   expenses = mainExpenses + secExpenses;  <----because function do not read variables?
+   return expenses;
+}*/
+console.log('Expenses: ' + expenses); 
+
+//4.2, 4.3
+let accumulatedMonth = '';
+function getAccumulatedMonth(a, b){
+   accumulatedMonth = a - b;
+   return accumulatedMonth; 
 }
+getAccumulatedMonth(money, expenses);
+console.log('Monthly savings: ' + accumulatedMonth + "£");
+
+//4.4
+function getTargetMonth (a, b){
+   period = Math.ceil(a / b);
+   return period;
+}
+getTargetMonth(mission, accumulatedMonth);
+console.log('Goal will be achieved in ' + period + ' months');
+//4.5
+
+budgetPerDay = Math.floor(accumulatedMonth/30);
+console.log( 'Your budget per day: ' + budgetPerDay + "£");
+
