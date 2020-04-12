2e07712397b73f78090c11001d7e3c46c427e6e5
JustineGoroshenko <justinegoroshenko@yahoo.com>
2020-04-12 15:04:31 +0100

    lesson09 added

--- Glo/script.js
+++ Glo/script.js
@@ -1,5 +1,34 @@
 'use strict';
 
+//a
+let btnCount = document.querySelector('#start'),
+    btnCancel = document.querySelector('#cancel'),
+//b   
+   btnPlusIncome = document.querySelector('button.income_add'),
+   btnPlusExpenses = document.querySelector('button.expenses_add'),
+//c
+   depositCheck = document.querySelector('#deposit-check'),
+//d
+   additionalIncomeItems = document.querySelectorAll('.additional_income-item'),
+//e
+   budgetMonthValue = document.querySelector('.budget_month-value'),
+   budgetDayValue = document.querySelector('.budget_day-value'),
+   expensesMonthValue = document.querySelector('.expenses_month-value'),
+   additionalIncomeValue = document.querySelector('.additional_income-value'),
+   additionalExpensesValue = document.querySelector('.additional_expenses-value'),
+   incomePeriodValue = document.querySelector('.income_period-value'),
+   targetMonthValue = document.querySelector('.target_month-value'),
+//f 
+   inputSalaryAmount = document.querySelector('.salary-amount'),
+   incomeTitle = document.querySelector('income-title'),
+   incomeAmount = document.querySelector('income-amount'),
+   expensesTitle = document.querySelector('.expenses-title'),
+   expensesAmount = document.querySelector('.expenses-amount'),
+   additionalExpensesItem = document.querySelector('.additional_expenses-item');
+
+
+   
+
 let isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);  
 }
